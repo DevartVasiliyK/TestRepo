48ff18e46238b893142a993eb9a85709ea55b4d1
rrr <git@github.com>
2015-08-19 11:20:37 +0300

    1

--- Tables/dbo.table1.sql
+++ Tables/dbo.table1.sql
@@ -1,6 +1,10 @@
 ﻿
 CREATE TABLE [dbo].[table1] (
-  [d] int NULL
+  [id] [int] IDENTITY,
+  [column1] [varchar](50) NULL,
+  [column3] [varchar](50) NULL,
+  [column2] [varchar](50) NULL,
+  CONSTRAINT [PK_table1] PRIMARY KEY CLUSTERED ([id])
 )
 ON [PRIMARY]
 GO
