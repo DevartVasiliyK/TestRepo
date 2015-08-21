7f20a7a1854aafc6631e7ee5772293fae4ba4bca
rrr <git@github.com>
2015-08-21 10:21:37 +0300

    1

---
+++ Tables/dbo_t_second.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[t_second] (
+  [pk] [int] NOT NULL,
+  [fk] [int] NULL,
+  [txt_0] [nvarchar](max) NULL,
+  [txt_1] [nvarchar](max) NULL,
+  [txt_2] [nvarchar](max) NULL,
+  [txt_3] [nvarchar](max) NULL,
+  [txt_4] [nvarchar](max) NULL,
+  [txt_5] [nvarchar](max) NULL,
+  [txt_6] [nvarchar](max) NULL,
+  [txt_7] [nvarchar](max) NULL,
+  [txt_8] [nvarchar](max) NULL,
+  [txt_9] [nvarchar](max) NULL,
+  PRIMARY KEY CLUSTERED ([pk]),
+  CONSTRAINT [fk_to_first] FOREIGN KEY ([fk]) REFERENCES [dbo].[t_first] ([pk])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
