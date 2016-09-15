b2438ce8985514e4da55a064c2c33c69dce8e2de
DmitryB <dmitryb@devart>
2016-09-15 14:02:01 +0300

    1

---
+++ Tables/dbo_table1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[table1] (
+  [id] [int] IDENTITY,
+  [col1] [sql_variant] NULL,
+  [col2] [sql_variant] NULL,
+  CONSTRAINT [PK_table1] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
