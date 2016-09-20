b006e435e7d8d1e8123d9d216b6a68bd0d1156a1
dd <dddf@df>
2016-09-20 05:45:31 -0700

    2

--- Tables/dbo_table1.sql
+++ Tables/dbo_table1.sql
@@ -1,8 +1,17 @@
 ﻿CREATE TABLE [dbo].[table1] (
-  [id] [int] IDENTITY,
-  [col1] [sql_variant] NULL,
-  [col2] [sql_variant] NULL,
-  CONSTRAINT [PK_table1] PRIMARY KEY CLUSTERED ([id])
+  [ф] [varchar](50) NULL,
+  [с1] [varchar](50) NULL
 )
 ON [PRIMARY]
+GO
+
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE TRIGGER [trigger1]
+ON [dbo].[table1]
+FOR INSERT
+AS SELECT ''
+GO
+
+DISABLE TRIGGER [dbo].[trigger1] ON [dbo].table1
 GO
