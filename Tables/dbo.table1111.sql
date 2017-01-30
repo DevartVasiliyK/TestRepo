12316a5e2154e64ab14dbec0f355d546516c16e3
test < test@devart>
2017-01-30 12:48:25 +0200

    add mod

--- Tables/dbo.table1111.sql
+++ Tables/dbo.table1111.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[table1111] (
-  [c1] [varchar](50) NULL,
+  [c1] [varchar](50) COLLATE Cyrillic_General_CI_AS NULL,
   [c2] [varchar](50) NULL
 )
 ON [PRIMARY]
 GO
 
-EXEC sys.sp_addextendedproperty N'MS_Description', N'test1', 'SCHEMA', N'dbo', 'TABLE', N'table1111', 'COLUMN', N'c1'
+EXEC sys.sp_addextendedproperty N'MS_Description', N'test11111', 'SCHEMA', N'dbo', 'TABLE', N'table1111', 'COLUMN', N'c1'
 GO
 
 EXEC sys.sp_addextendedproperty N'MS_Description', N'test2', 'SCHEMA', N'dbo', 'TABLE', N'table1111', 'COLUMN', N'c2'
