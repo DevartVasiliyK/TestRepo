e4a61a21c488e13e0201a5c27866045019fddb91
test < test@devart>
2017-01-30 12:46:55 +0200

    add1

---
+++ Tables/dbo.table1111.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[table1111] (
+  [c1] [varchar](50) NULL,
+  [c2] [varchar](50) NULL
+)
+ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'test1', 'SCHEMA', N'dbo', 'TABLE', N'table1111', 'COLUMN', N'c1'
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'test2', 'SCHEMA', N'dbo', 'TABLE', N'table1111', 'COLUMN', N'c2'
+GO
