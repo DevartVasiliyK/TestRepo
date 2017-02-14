17cef0867003386d0e065fb281d68826630bac0f
test <test@devart>
2017-02-14 14:19:20 +0200

    initial commit

---
+++ Tables/HumanResources.table1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [HumanResources].[table1] (
+  [column1] [varchar](50) NULL,
+  [column2] [varchar](50) NULL,
+  [column3] [varchar](50) NULL,
+  [column4] [varchar](50) NULL,
+  [column5] [varchar](50) NULL
+)
+ON [PRIMARY]
+GO
