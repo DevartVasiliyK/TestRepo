31934e6188fd3d2a597d41a63874c5d8e479dda7
VasiliyK <VasiliyK@Devart.com>
2017-06-22 14:57:42 +0300

    tst

---
+++ Tables/dbo.ActivationConfig.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ActivationConfig] (
+  [Name] [varchar](50) NULL,
+  [SiteCode] [varchar](50) NULL,
+  [Version] [varchar](50) NULL
+)
+ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'test', 'SCHEMA', N'dbo', 'TABLE', N'ActivationConfig'
+GO
