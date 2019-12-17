8f5dd24423ea756d7bb5e7a2bffc914125c8c1b1
vladk <vladk@devart.com>
2019-12-17 12:48:56 +0200

    2

---
+++ Tables/dbo.configuration_properties.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[configuration_properties] (
+  [id] [varchar](50) NOT NULL,
+  [key] [varchar](50) NOT NULL,
+  [value] [nvarchar](4000) NULL,
+  [default] [nvarchar](4000) NULL,
+  [protection] [smallint] NOT NULL,
+  [access] [smallint] NOT NULL,
+  [datatype] [varchar](50) NOT NULL,
+  CONSTRAINT [PK_configuration_properties] PRIMARY KEY CLUSTERED ([id], [key])
+)
+ON [PRIMARY]
+GO
