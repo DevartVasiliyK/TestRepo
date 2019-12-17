e31424ff31466d76dd382542a29beb5fc5c490a7
vladk <vladk@devart.com>
2019-12-17 12:47:49 +0200

    2

---
+++ Tables/dbo.compute_node.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[compute_node] (
+  [id] [int] NOT NULL,
+  [name] [varchar](32) NOT NULL,
+  [address] [varchar](15) NOT NULL,
+  [database] [varchar](30) NOT NULL DEFAULT ('master'),
+  [instance] [varchar](16) NULL DEFAULT ('SQLSERVER'),
+  [active] [bit] NOT NULL,
+  [index] [int] NOT NULL,
+  [driveletter] [varchar](2) NOT NULL,
+  CONSTRAINT [PK_compute_node] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
