a6e8a8bee37728be3fd23f95dd0bc3ab939e7696
Василий Климушев <VasiliyK@Devart.com>
2021-01-05 15:09:56 +0200

    pass3

--- Tables/dbo.table1.sql
+++ Tables/dbo.table1.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[table1] (
   [id1] [int] IDENTITY,
   [c2] [varchar](50) NULL,
+  [c3] [varchar](50) NULL,
   CONSTRAINT [PK_table1] PRIMARY KEY CLUSTERED ([id1])
 )
 ON [PRIMARY]
