5dad67cd68b4a67cafd7e0b27ddb67285fad9505
Василий Климушев <VasiliyK@Devart.com>
2021-01-27 14:42:51 +0200

    ta3

---
+++ Tables/dbo.emp_copy.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[emp_copy] (
+  [id] [int] NOT NULL,
+  [name] [varchar](200) NULL,
+  [dept] [int] NULL,
+  PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[emp_copy]
+  ADD FOREIGN KEY ([dept]) REFERENCES [dbo].[dept] ([id])
+GO
